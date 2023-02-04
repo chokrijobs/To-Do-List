3c42c5ba82bda3431017ea8a6d08bf4ac7a5fc23
Chokri Selmi <selmi.chokri@gmail.com>
2023-02-04 20:46:59 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TaskList from './TaskList';
+import NewTaskForm from './NewTaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, description: 'Buy groceries', completed: false },
+    { id: 2, description: 'Do laundry', completed: false },
+    { id: 3, description: 'Write blog post', completed: true },
+  ]);
+
+  const addTask = (description) => {
+    setTasks([...tasks, { id: Date.now(), description, completed: false }]);
+  };
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      })
+    );
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <NewTaskForm addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        toggleTaskCompletion={toggleTaskCompletion}
+        deleteTask={deleteTask}
+      />
     </div>
   );
 }
---
+++ src/TaskList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Task from './Task';
+
+const TaskList = ({ tasks, toggleTaskCompletion, deleteTask }) => (
+    <ul>
+        {tasks.map((task) => (
+            <Task
+                key={task.id}
+                task={task}
+                toggleTaskCompletion={toggleTaskCompletion}
+                deleteTask={deleteTask}
+            />
+        ))}
+    </ul>
+);
+
+export default TaskList;
